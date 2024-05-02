1c6fc8383304a00203380f34f0928d6cd91a46cf
Shijie Lian <89021551+LiamLian0727@users.noreply.github.com>
2024-05-02 16:36:54 +0800

    Update datasets.py

--- project/our/our_model/datasets.py
+++ project/our/our_model/datasets.py
@@ -3,27 +3,6 @@ from mmdet.registry import DATASETS
 
 
 @DATASETS.register_module()
-class ForegroundUIIS10KInsSegDataset(CocoDataset):
-    """Dataset for Cityscapes."""
-
-    METAINFO = {
-        'classes': ['foreground'],
-        'palette': [(0, 0, 255)]
-    }
-
-
-@DATASETS.register_module()
-class MultiClassUIIS10KInsSegDataset(CocoDataset):
-    """Dataset for Cityscapes."""
-
-    METAINFO = {
-        'classes': ['fish', 'reptiles', 'arthropoda', 'corals', 'mollusk',
-                    'plants', 'ruins', 'garbage', 'human', 'robots'],
-        'palette': [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230), (106, 0, 228),
-                    (0, 60, 100), (0, 80, 100), (0, 0, 70), (0, 0, 192), (250, 170, 30)]
-    }
-
-@DATASETS.register_module()
 class ForegroundUSIS10KInsSegDataset(CocoDataset):
     """Dataset for Cityscapes."""
 

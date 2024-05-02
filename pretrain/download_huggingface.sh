4c1bc4ed5f98ea2cf8b58766f0d188c6743ab3fc
Shijie Lian <89021551+LiamLian0727@users.noreply.github.com>
2024-05-02 22:00:24 +0800

    Update download_huggingface.sh

--- pretrain/download_huggingface.sh
+++ pretrain/download_huggingface.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 NAME=$1
+SAVEDIR=$2
 
 export HF_ENDPOINT="https://hf-mirror.com"
 
 huggingface-cli download \
 --resume-download $NAME \
+--cache-dir $SAVEDIR \
 --exclude *.safetensors *.h5 \
 ${@:3}

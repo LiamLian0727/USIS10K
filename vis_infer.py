71b1f0fa8efdb2695607cb6023f2e038ec623725
Shijie Lian <89021551+LiamLian0727@users.noreply.github.com>
2025-02-07 00:21:41 +0800

    Update vis_infer.py

--- vis_infer.py
+++ vis_infer.py
@@ -1,7 +1,7 @@
 from mmdet.apis import DetInferencer
 
 def vis_infer(
-    checkpoints="./pretrain/multi_class_model_with_classes.pth",
+    checkpoints="./pretrain/multi_class_model.pth",
     config='./project/our/configs/multiclass_usis_train.py',
     data_dir='./data/USIS10K/test/',
     output_dir='./USIS10K/data/vis/test'
@@ -9,7 +9,7 @@ def vis_infer(
     """
     Function to run the DetInferencer for visual inference with default parameters.
     Args:
-    checkpoints (str): Path to the model checkpoint (default: "./pretrain/multi_class_model_with_classes.pth").
+    checkpoints (str): Path to the model checkpoint (default: "./pretrain/multi_class_model.pth").
     config (str): Path to the configuration file (default: './project/our/configs/multiclass_usis_train.py').
     data_dir (str): Path to the directory containing the test data (default: './data/USIS10K/test/').
     output_dir (str): Path to the output directory where results will be saved (default: './USIS10K/data/vis/test').
